3e736dd5dc336bfeb4ec7f0abc823518bbd282d3
Dastan <96249607+tamashi22@users.noreply.github.com>
2022-04-19 20:58:00 +0600

    v

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import "./App.css";
 import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
       <header>
-      <Header/>
+        <Header />
       </header>
-     
     </div>
   );
 }
---
+++ src/redux/reposReducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+  item: [],
+  isFetching: true
+};
+
+export default function reposReducer(state = defaultState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
